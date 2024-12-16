1e2f8a530b2ee4716cd7a2e5beac9d85eb9c133c
Patrik Drevestedt <patrik.drevestedt@me.com>
2024-12-16 16:29:23 +0100

    add function for places choice

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,43 @@
-// Getting user input and displaying package
+// Display Holiday Package based on user input
 document.addEventListener("DOMContentLoaded", () => {
-  const creatorForm = document.getElementById("creator-form");
-  const displayPackage = document.getElementById("display-package");
-  const packageCity = document.getElementById("package-city");
-  const packageAccomodation = document.getElementById("package-accomodation");
-  const packagePrice = document.getElementById("package-price");
-  const packagePlaces = document.getElementById("package-places");
+  const form = document.getElementById("creator-form");
 
-  // Prevent form from reloading the page
-  creatorForm.addEventListener("submit", (e) => {
+  // Prevent default form submission
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
   })
 
-  // Get user input values 
-  const city = document.getElementById("city");
-  const accomodation = document.getElementById("accomodation");
-  const priceRange = document.getElementById("price-range");
-  const places = document.getElementById("places");
+  // Collect user input 
+  const city = document.getElementById("city-choice");
+  const placesForCity = document.getElementById("places-choice");
+  const accomodation = document.getElementById("accomodation-choice");
+  const price = document.getElementById("price-choice");
 
-  // Validate inputs 
-  if (!city || !accomodation || !priceRange || !places) {
-
+  // Places to choose from based on city selection
+  const cityPlaces = {
+    Miami: ["Miami Beach", "Little Havanna", "Art Deco Historic District", "Everglades National Park", "Bayside Market Place"],
+    Orlando: ["Disney World", "Universal Studios", "Sea World", "Kennedy Space Center", "Ripley's Believe It or Not!"],
+    Fort_Meyers_Beach: ["Sunset Dolphin Cruise", "Lovers Key State Park", "Sanibel Island"],
+    Key_West: ["Sloppy Joes", "The Ernest Hemingway Home and Museum", "Key West Lighthouse and Keeper's Quarters Museum"],
+    Tampa: ["Bush Gardens", "The Florida Aquarium", "Ybor City"]
   }
-})
+
+  // Event listener for City selection 
+  city.addEventListener("change", () => {
+    const selectedCity = city.value;
+
+    // Clear existing option in Places dropdown 
+    placesForCity.innerHTML = `<option value="" disabled selected>Select a city</option>`;
+
+    // Get places for the selected city 
+    const availablePlaces = cityPlaces[selectedCity] || [];
+
+    // Populate Places Dropdown with new options 
+    places.forEach(place => {
+      const option = document.createElement("option");
+      option.value = place;
+      option.textContent = place;
+      placesForCity.appendChild("option");
+    });
+  });
+});
